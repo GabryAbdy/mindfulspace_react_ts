7bdcf12efa57b40f83399aa2d2523abc93ecf1ae
Gabriele Abd Alla Awad <abdallaawad.gabriele@gmail.com>
2026-07-28 17:36:41 +0200

    docs: add section comments to improve code readability

--- src/features/sounds/useSounds.ts
+++ src/features/sounds/useSounds.ts
@@ -7,16 +7,24 @@ import type {
 import { getSoundById } from "../../services/freesoundApi";
 
 export default function useSounds(soundIds: number[]) {
+  // Hook state
+
   const [isLoading, setIsLoading] = useState(true);
   const [fetchResults, setFetchResults] = useState<SoundFetchResult[]>([]);
 
   useEffect(() => {
+    // Fetch sounds metadata
+
     async function fetchSounds() {
       setIsLoading(true);
 
+      // Exectue all requests in parallel,
+      // keeping track of both successes and failures
       const settledResults = await Promise.allSettled(
         soundIds.map((id) => getSoundById(id)),
       );
+
+      // Normalize API responses
 
       const results: SoundFetchResult[] = settledResults.map(
         (result, index) => {
@@ -42,12 +50,17 @@ export default function useSounds(soundIds: number[]) {
         },
       );
 
+      // Update hook state
+
       setFetchResults(results);
       setIsLoading(false);
     }
 
+    // Call async fetch without awaiting it
     void fetchSounds();
   }, [soundIds]);
+
+  // Public hook API
 
   return { isLoading, fetchResults };
 }
