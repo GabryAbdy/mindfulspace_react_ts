f6f3eee7e7697fa4d99a7d644eede168599cdaa9
Gabriele Abd Alla Awad <abdallaawad.gabriele@gmail.com>
2026-08-07 18:02:32 +0200

    fix(sounds): use dedicated icon for silence option instead of shared play button

--- src/features/sounds/NoSoundOption.tsx
+++ src/features/sounds/NoSoundOption.tsx
@@ -1,4 +1,6 @@
 import Card from "../../components/ui/Card";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeXmark } from "@fortawesome/free-solid-svg-icons";
 
 interface NoSoundOptionProps {
   isSelected: boolean;
@@ -11,9 +13,10 @@ export default function NoSoundOption({
 }: NoSoundOptionProps) {
   return (
     <Card isSelected={isSelected} onClick={onSelect}>
-      <div className="flex h-10 w-10 shrink-0 items-center justify-center rounded-full bg-grass-700 text-white">
-        ✕
-      </div>
+      <FontAwesomeIcon
+        icon={faVolumeXmark}
+        className="flex h-10 w-10 shrink-0 items-center justify-center text-cream-700"
+      />
       <div className="font-medium text-cream-700">Silence</div>
     </Card>
   );
