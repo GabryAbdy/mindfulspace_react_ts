c70620c845aed205ca2afccf352f5babc562ea05
Gabriele Abd Alla Awad <abdallaawad.gabriele@gmail.com>
2026-08-25 13:11:35 +0200

    style(layout): implement mobile-first desktop frame pattern in PageWrapper

--- src/components/layout/PageWrapper.tsx
+++ src/components/layout/PageWrapper.tsx
@@ -5,5 +5,9 @@ interface PageWrapperProps {
 }
 
 export default function PageWrapper({ children }: PageWrapperProps) {
-  return <div>{children}</div>;
+  return (
+    <div className="w-full md:max-w-md mt-2 md:mx-auto md:border md:rounded-2xl md:border-sand-300 p-4 md:bg-cream-500 md:shadow-md">
+      {children}
+    </div>
+  );
 }
