5d822fa231cb47c4d6daef1ae414a5d4d8aff926
Gabriele Abd Alla Awad <abdallaawad.gabriele@gmail.com>
2026-08-26 13:00:35 +0200

    style(SoundPicker): enhance layout and button styles for improved user experience

--- src/features/sounds/SoundPicker.tsx
+++ src/features/sounds/SoundPicker.tsx
@@ -89,7 +89,10 @@ export default function SoundPicker() {
   }
 
   return (
-    <div>
+    <div className="flex flex-col">
+      <h2 className="mb-5 px-4 border border-grass-700 bg-grass-300 rounded-lg font-bold text-lg shadow-sm self-center">
+        Select your ambient sound
+      </h2>
       {/* Sound Options */}
       <div
         onKeyDown={handleRadioGroupKeyDown}
@@ -133,7 +136,7 @@ export default function SoundPicker() {
         type="button"
         disabled={isLoading}
         onClick={handleConfirm}
-        className="rounded bg-grass-700 border p-2 m-1 cursor-pointer"
+        className="self-center w-25 h-12 mt-5 p-2 rounded-2xl border border-grass-700 bg-grass-500 text-lg font-bold shadow-md cursor-pointer transition-all duration-150 hover:bg-grass-700 active:scale-90 disabled:cursor-not-allowed disabled:opacity-50"
       >
         Confirm
       </button>
--- src/pages/Sounds.tsx
+++ src/pages/Sounds.tsx
@@ -1,10 +1,5 @@
 import SoundPicker from "../features/sounds/SoundPicker";
 
 export default function Sounds() {
-  return (
-    <div>
-      <h2>Select your ambient sound</h2>
-      <SoundPicker />
-    </div>
-  );
+  return <SoundPicker />;
 }
