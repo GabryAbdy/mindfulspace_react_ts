2c0b5357439b573c06db91682458e9db4bccf695
Gabriele Abd Alla Awad <abdallaawad.gabriele@gmail.com>
2026-08-27 18:47:10 +0200

    style(PageWrapper): adjust margin for improved layout consistency

--- src/components/layout/PageWrapper.tsx
+++ src/components/layout/PageWrapper.tsx
@@ -6,7 +6,7 @@ interface PageWrapperProps {
 
 export default function PageWrapper({ children }: PageWrapperProps) {
   return (
-    <div className="w-[calc(100%-0.5rem)] max-w-md mt-2 mx-auto border rounded-2xl text-[clamp(0.950rem,2vw,1.125rem)] border-sand-700 p-4 bg-sand-300 shadow-md">
+    <div className="w-[calc(100%-0.5rem)] max-w-md my-2 mx-auto border rounded-2xl text-[clamp(0.950rem,2vw,1.125rem)] border-sand-700 p-4 bg-sand-300 shadow-md">
       {children}
     </div>
   );
