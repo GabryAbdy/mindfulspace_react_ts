44368704a7eefd471643482336c577908d6d6251
Gabriele Abd Alla Awad <abdallaawad.gabriele@gmail.com>
2026-08-27 12:09:12 +0200

    style(Header): adjust padding and font styles for improved header appearance

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,10 +3,10 @@ import { Link } from "react-router";
 export default function Header() {
   return (
     <header className="bg-cream-700 border-b">
-      <div className="flex justify-center py-4">
+      <div className="flex justify-center py-3">
         {/* Brand */}
-        <Link to="/" className="font-black text-2xl">
-          mindfulspace
+        <Link to="/" className="font-light text-3xl">
+          mindful*space
         </Link>
       </div>
     </header>
